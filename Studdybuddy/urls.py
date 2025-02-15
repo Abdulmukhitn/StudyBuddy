e94563d5e78d34d5f669f8ac1592905e97101342
Abdulmukhitx <a_nurkazy@kbtu.kz>
2025-02-15 17:53:53 +0500

    Added superuser

--- Studdybuddy/urls.py
+++ Studdybuddy/urls.py
@@ -1,9 +1,11 @@
+from django import core
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),
 
 ]
